c4f8452024e871b0027ca0eb8a850a5129ee3032
harmansingh25 <55681620+harmansingh25@users.noreply.github.com>
2021-02-18 19:31:42 +0530

    Update Completed_Projects.sql

--- Completed_Projects.sql
+++ Completed_Projects.sql
@@ -1,10 +1,12 @@
 use AVAAs;
 create table Completed_Projects (
-  ID int not null,
-  location varchar(20),
-  size int,
-  price float(2),
-  primary key(ID)
+	ID int not null,
+	location varchar(200) not null,
+    size float(2) check(size>0) not null, 
+    price float(2) check (price>0) not null, /*In crores*/
+    primary key(ID)
 );
-
-
+insert into Completed_Projects values(1,'110060 Old Rajinder Nagar',11.23, 1.20);
+insert into Completed_Projects values(2,'110008 West Patel Nagar',12.21, 2.10);
+insert into Completed_Projects values(3,'110031 Shastri Nagar',15.12, 2.80);
+insert into Completed_Projects values(4,'110015 West Ramesh Nagar',14.21, 2.60);
