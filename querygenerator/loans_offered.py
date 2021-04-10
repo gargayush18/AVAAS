334ce7da64c958fad0252a1797c4bd30c9a9bb6f
ayush19030 <ayush19030@iiitd.ac.in>
2021-04-10 23:50:57 +0530

    input queries

---
+++ querygenerator/loans_offered.py
@@ -0,0 +1,54 @@
+# Program to extract a particular row value
+
+import openpyxl 
+import random
+  
+# Give the location of the file 
+
+  
+# workbook object is created 
+  
+# Loop will print all columns name 
+
+path = "loans_offered.xlsx"
+wb_obj = openpyxl.load_workbook(path) 
+  
+sheet_obj = wb_obj.active 
+
+
+
+
+#INSERT INTO loans VALUES(1,'Tapan Roy','SBI',4.8,'2016-12-14',500000.0,200000.0);
+#INSERT INTO contractors VALUES (1,'Boris Singh','1960-01-01',12456889,50,'NO',null,null);
+
+mainans=""
+
+xx=1 ;
+xy =  52; 
+for i in range(1,100):
+	ans=""
+
+	for j in range(1,8):
+		cell_obj = sheet_obj.cell(row = i, column = j) 
+		oho=cell_obj.value
+		trr=oho
+		oho= str(oho)
+		if(isinstance(trr, int) or isinstance( trr, float )):
+			ans+=str(cell_obj.value)+","
+		
+		else:
+			oho=oho.replace("\n"," ")
+			oho=oho.replace("  ","")
+			oho=oho.replace("00:00:00","")
+			ans+="'"+str(oho)+"'"+","
+	
+	mainans+="INSERT INTO loans_offered VALUES("+ans[:-1]+");\n"
+	xx+=1
+	xy+=1
+
+    
+
+print(mainans)
+
+
+
