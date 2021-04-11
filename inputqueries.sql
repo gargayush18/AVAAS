d29dc6b350f6c28e2cfc85be650131bddadad475
ayush19030 <ayush19030@iiitd.ac.in>
2021-04-11 21:41:07 +0530

    new inputs

--- inputqueries.sql
+++ inputqueries.sql
@@ -1,3 +1,5 @@
+
+
 INSERT INTO Financial_Customers VALUES('FICT001','M/s Recon Engineers(India) Pvt. Ltd. 227,Kamalalaya Centre,156 A Lenin sarani,Kolkata 700013','contractor',true,83);
 INSERT INTO Financial_Customers VALUES('FICT002','M/s. Durha Construction Pvt. Ltd 5th Floor, Anushika Shopping Mall Sector 11, Rohini New Delhi-110085','contractor',true,83);
 INSERT INTO Financial_Customers VALUES('FICT003','M/s. Vensar Constructions Company Limited, Plot no. 28, 1st Floor, Navodaya Colony, Road No 2, Banjara hills, Hyderabad 500034, Andhra Pradesh','contractor',true,94);
