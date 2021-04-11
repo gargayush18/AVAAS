123995a29e23e4847dd5252342b7087981ab35d3
ayush19030 <ayush19030@iiitd.ac.in>
2021-04-11 21:40:32 +0530

    new input

--- querygenerator/contractor_competency.py
+++ querygenerator/contractor_competency.py
@@ -18,15 +18,25 @@ mainans=""
 
 
 
-for i in range(1,41):
-	x = random.randint(1,2)
+# for i in range(1,41):
+# 	x = random.randint(1,2)
+# 	gt= ""
+# 	if ( x==2):
+# 		gt="YES"
+# 	else:
+# 		gt="NO"
+# 	mainans+="INSERT INTO project_requirements VALUES('ONGP00"+str(i)+"',"+str(random.randint(5,20))+",'"+gt+"',"+str(random.randint(50,200 ))+","+str(random.randint(10,40))+");\n"
+
+for i in range(1,58):
+	x = random.randint(1,3)
 	gt= ""
 	if ( x==2):
-		gt="YES"
-	else:
-		gt="NO"
-	mainans+="INSERT INTO project_requirements VALUES('ONGP00"+str(i)+"',"+str(random.randint(5,20))+",'"+gt+"',"+str(random.randint(50,200 ))+","+str(random.randint(10,40))+");\n"
-
+		gt="A"
+	if ( x==1):
+		gt="B"
+	if( x==3):
+		gt="C"
+	mainans+="INSERT INTO public_competence VALUES('PUB00"+str(i)+"','"+gt+"',"+str(x)+",'"+"YES"+"'"+");\n"
 
   
 # workbook object is created 
